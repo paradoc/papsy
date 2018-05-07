158d711179e7777e33ade1bad5e316369a56991c
paradoc <coprada.mj@gmail.com>
2018-05-08 03:44:00 +0800

    Added dialog box on day click

--- src/client/containers/Schedule.jsx
+++ src/client/containers/Schedule.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import DayPicker from 'react-day-picker';
+import SkyLight from 'react-skylight';
 
 import 'react-day-picker/lib/style.css';
 import Back from '../assets/Back';
 import '../styles/Main.css';
+
+const styles = {
+  marginLeft: '-42%',
+  marginRight: '10%',
+  width: '75%',
+};
 
 class Schedule extends React.Component {
   state = {
     date: new Date(),
   }
 
-  onChange(date) {
-    this.setState({ date });
-  }
-
   render() {
     console.log(this.state.date);
+    const displayModal = (date) => {
+      this.setState({ date });
+      this.modal.show();
+    };
     return (
       <div className="main-container">
         <div className="back-button-container">
@@ -27,7 +34,12 @@ class Schedule extends React.Component {
               Select your desired appointment schedule
             </span>
           </div>
-          <DayPicker className="calendar" />
+          <DayPicker className="calendar" onDayClick={displayModal} />
+          <SkyLight
+            ref={(ref) => { this.modal = ref; }}
+            dialogStyles={styles}
+            title="Select a timeslot"
+          />
         </div>
       </div>
     );
