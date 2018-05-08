0594a0be3175ba560f6ec01b2f2593f8ed23c9a4
paradoc <coprada.mj@gmail.com>
2018-05-09 01:56:59 +0800

    Fixed JSON conversion on SMS sending

--- src/server/sms.php
+++ src/server/sms.php
@@ -17,10 +17,10 @@ class SemaphoreSMS
     $api_key = '375ab33ac1f1b23ab1276f84e68b5cf0';
 
     $body = array(
-      'apikey' => $api_key,
-      'number' => $number,
-      'message' => $message,
-      // 'sendername' => 'PApSy',
+      "apikey" => "$api_key",
+      "number" => "$number",
+      "message" => "$message",
+      // "sendername" => "PApSy",
     );
 
     // Initiate cURL.
@@ -30,7 +30,7 @@ class SemaphoreSMS
     curl_setopt($ch, CURLOPT_POST, 1);
 
     // Attach our encoded JSON to the POST fields.
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($body));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
 
     // Set the content type to application/json.
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
